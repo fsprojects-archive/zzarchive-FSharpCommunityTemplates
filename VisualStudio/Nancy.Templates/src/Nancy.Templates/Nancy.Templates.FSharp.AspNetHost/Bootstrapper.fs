a0ade3b5592df16104aa4a8088f9d36aead770f0
Dan Mohl <dmohl@yahoo.com>
2013-11-28 17:13:07 -0600

    Added Nancy.Templates

---
+++ VisualStudio/Nancy.Templates/src/Nancy.Templates/Nancy.Templates.FSharp.AspNetHost/Bootstrapper.fs
@@ -0,0 +1,10 @@
+﻿namespace Nancy.Templates.FSharp.AspNetHost
+
+open Nancy
+
+type Bootstrapper() =
+    inherit DefaultNancyBootstrapper()
+    // The bootstrapper enables you to reconfigure the composition of the framework,
+    // by overriding the various methods and properties.
+    // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+
