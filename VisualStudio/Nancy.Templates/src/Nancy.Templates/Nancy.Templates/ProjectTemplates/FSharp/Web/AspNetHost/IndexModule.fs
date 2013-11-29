a9c0cbcef3e6ac1a14a8c60f8d075d2e5a3042e2
Dan Mohl <dmohl@yahoo.com>
2013-11-28 19:35:35 -0600

    Continued working on adding the Nancy templates

---
+++ VisualStudio/Nancy.Templates/src/Nancy.Templates/Nancy.Templates/ProjectTemplates/FSharp/Web/AspNetHost/IndexModule.fs
@@ -0,0 +1,8 @@
+﻿namespace $safeprojectname$
+
+open Nancy
+
+type IndexModule() as x =
+    inherit NancyModule()
+    do x.Get.["/"] <- fun _ -> box x.View.["index"]
+
---
+++ VisualStudio/Nancy.Templates/src/Nancy.Templates/Nancy.Templates/ProjectTemplates/FSharp/Web/CsFsAspNet/Nancy.Templates.FSharp.AspNetHost/IndexModule.fs
@@ -0,0 +1,8 @@
+﻿namespace Nancy.Templates.FSharp.AspNetHost
+
+open Nancy
+
+type IndexModule() as x =
+    inherit NancyModule()
+    do x.Get.["/"] <- fun _ -> box x.View.["index"]
+
