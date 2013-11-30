137dcddc1e7609761f465c69eb61b83a1ad2c0d0
Dan Mohl <dmohl@yahoo.com>
2013-11-29 19:12:29 -0600

    Removed the unzipped versions of the templates.

--- VisualStudio/Nancy.Templates/src/Nancy.Templates/Nancy.Templates/ProjectTemplates/FSharp/Web/Demo/Bootstrapper.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace $safeprojectname$
-
-open Nancy
-
-type Bootstrapper() =
-    inherit DefaultNancyBootstrapper()
-    // The bootstrapper enables you to reconfigure the composition of the framework,
-    // by overriding the various methods and properties.
-    // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
--- VisualStudio/Nancy.Templates/src/Nancy.Templates/Nancy.Templates/ProjectTemplates/FSharp/Web/SelfHost/IndexModule.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace $safeprojectname$
-
-open Nancy
-
-type IndexModule() as x =
-    inherit NancyModule()
-    do x.Get.["/"] <- fun _ -> box x.View.["index"]
-
