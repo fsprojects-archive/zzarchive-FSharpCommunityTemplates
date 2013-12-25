b8f7b9e618c9df2113f6b30272d6c2bbc5defced
James D'Angelo <james@thegeekco.com>
2013-12-24 22:41:18 -0800

    Add initial Simple.Web project template.

---
+++ VisualStudio/Simple.Web/TemplatePack/ProjectTemplates/Web/Simple.Web/Home.fs
@@ -0,0 +1,10 @@
+﻿namespace $safeprojectname$.Home
+
+open Simple.Web
+
+[<UriTemplate("/")>]
+type Index() as this =
+    member __.Get() = Status 200
+
+    interface IGet with
+        member __.Get() = this.Get()
---
+++ VisualStudio/Simple.Web/TemplatePack/ProjectTemplates/Web/Simple.Web/OwinAppSetup.fs
@@ -0,0 +1,26 @@
+﻿module OwinAppSetup
+
+open System
+open System.Collections.Generic
+open System.Threading.Tasks
+open Simple.Web
+
+type UseAction = Action<Func<IDictionary<string, obj>, Func<IDictionary<string, obj>, Task>, Task>>
+
+type OwinAppSetup() =
+    static member Setup(useMethod:UseAction) =
+        let run x y = Application.Run(x, y)
+        run |> useMethod.Invoke
+
+type StaticContentStartup() =
+    interface IStartupTask with
+        member __.Run(config, env) =
+            let pf f = config.PublicFolders.Add(PublicFolder f) |> ignore
+            ["/Scripts"; "/Content"; "/App"] |> List.iter pf
+
+open Simple.Web.StructureMap
+type StructureMapStartup() =
+    inherit StructureMapStartupBase()
+    override __.Configure(config) =
+        // config.For<IMySerivce>().Use<MyService>() |> ignore
+        ()
