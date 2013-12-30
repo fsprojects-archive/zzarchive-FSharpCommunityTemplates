e7db1a4120b59447ffa08b7f9aadaea37af3fbba
Dan Mohl <dmohl@yahoo.com>
2013-12-30 10:13:34 -0600

    Added a custom IWizard implementation to the MVC 5 template.

--- VisualStudio/Mvc5/FsMvc5TemplateWizard/TemplateWizard.fs
+++ VisualStudio/Mvc5/FsMvc5TemplateWizard/TemplateWizard.fs
@@ -6,9 +6,7 @@ open System.Windows.Forms
 open System.Collections.Generic
 open EnvDTE
 open EnvDTE80
-open Microsoft.VisualStudio.Shell
 open Microsoft.VisualStudio.TemplateWizard
-open VSLangProj
 open FsMvc5Dialog
 
 type TemplateWizard() =
@@ -16,11 +14,30 @@ type TemplateWizard() =
     [<DefaultValue>] val mutable dte : DTE
     [<DefaultValue>] val mutable dte2 : DTE2
     [<DefaultValue>] val mutable destinationPath : string
-    [<DefaultValue>] val mutable safeProjectName : string
     [<DefaultValue>] val mutable vsixInstallPath : string
-    [<DefaultValue>] val mutable isWebApi : bool
+    [<DefaultValue>] val mutable selectedProjectName : string
+    [<DefaultValue>] val mutable safeProjectName : string
 
-    let mutable selectedWebProjectName = "Razor"
+    let projectNames = [| "FsMvc5"; "WebApi" |]
+
+    let addProjects (templatePath:string) (dte2:DTE2) destinationPath selectedProjectName safeProjectName =
+        let currentCursor = Cursor.Current
+        Cursor.Current <- Cursors.WaitCursor
+        try
+            try
+                let AddProject status projectVsTemplateName projectName =
+                    dte2.StatusBar.Text <- status
+                    let path = templatePath.Replace("Web.fsharp.vstemplate", projectVsTemplateName)
+                    dte2.Solution.AddFromTemplate(path, destinationPath, projectName, false) |> ignore
+                AddProject "Adding the F# project..." 
+                    (Path.Combine(selectedProjectName, "MyTemplate.vstemplate")) safeProjectName
+            with
+            | ex -> 
+                failwith (sprintf "%s\n\r%s" "The project creation has failed. The actual exception message is: " ex.Message)
+        finally
+            Cursor.Current <- currentCursor
+
+
     interface IWizard with
         member this.RunStarted (automationObject:Object, 
                                 replacementsDictionary:Dictionary<string,string>, 
@@ -31,40 +48,18 @@ type TemplateWizard() =
             this.solution <- this.dte2.Solution :?> EnvDTE80.Solution2
             this.destinationPath <- replacementsDictionary.["$destinationdirectory$"]
             this.safeProjectName <- replacementsDictionary.["$safeprojectname$"]
-
             let dialog = new TemplateWizardDialog()
-            match dialog.ShowDialog().Value with
-            | true -> 
-                match dialog.SelectedProjectTypeIndex with
-                | 1 -> this.isWebApi <- true
-                | _ -> () 
-            | _ ->
-                raise (new WizardCancelledException())
+            try
+                match dialog.ShowDialog().Value with
+                | true -> 
+                    this.selectedProjectName <- projectNames.[dialog.SelectedProjectTypeIndex]
+                | _ ->
+                    raise (new WizardCancelledException())
+            with
+            | ex -> failwith (sprintf "%s\n\r%s" "The project creation has failed. The actual exception message is: " ex.Message)
         member this.ProjectFinishedGenerating project = "Not Implemented" |> ignore
         member this.ProjectItemFinishedGenerating projectItem = "Not Implemented" |> ignore
         member this.ShouldAddProjectItem filePath = true
         member this.BeforeOpeningFile projectItem = "Not Implemented" |> ignore
         member this.RunFinished() = 
-            let currentCursor = Cursor.Current
-            Cursor.Current <- Cursors.WaitCursor
-            try
-                let mutable selectedWebAppProjectName = "WebApp"
-
-                let templatePath = this.vsixInstallPath 
-                try
-                    let AddProject status projectVsTemplateName projectName =
-                        this.dte2.StatusBar.Text <- status
-                        let path = templatePath.Replace("FsMvc5.vstemplate", projectVsTemplateName)
-                        this.dte2.Solution.AddFromTemplate(path, Path.Combine(this.destinationPath, projectName), 
-                            projectName, false) |> ignore
-                    AddProject "Adding the F# Web project..." 
-                        (Path.Combine(selectedWebProjectName, selectedWebProjectName + ".vstemplate")) webName
-                    AddProject "Adding the F# Web project..." 
-                        (Path.Combine(selectedWebAppProjectName, selectedWebAppProjectName + ".vstemplate")) webAppName
-                with
-                | ex -> failwith (sprintf "%s\n\r%s" 
-                            "The project creation has failed. The actual exception message is: "
-                            ex.Message)
-            finally
-                Cursor.Current <- currentCursor
-            
+            addProjects this.vsixInstallPath this.dte2 this.destinationPath this.selectedProjectName this.safeProjectName
