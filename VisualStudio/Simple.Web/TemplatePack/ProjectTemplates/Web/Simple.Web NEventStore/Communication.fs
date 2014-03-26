bf8c9104cc8cc925ebe51bcc5db482b0ed25f52c
James D'Angelo <james@thegeekco.com>
2014-03-26 00:39:30 -0700

    Add new Simple.Web template with NEventStore
    Add new Simple.Web template that utilizes NEventStore and fills in a lot
    of the boiler-plate cruft usually needed to get started.

---
+++ VisualStudio/Simple.Web/TemplatePack/ProjectTemplates/Web/Simple.Web NEventStore/Communication.fs
@@ -0,0 +1,48 @@
+﻿namespace $safeprojectname$.Communication
+
+type IRouter<'a> =
+    abstract Send: 'a -> unit
+
+module Commands =
+    open System
+
+    let IssuedByKey = "IssuedBy"
+
+    type Issuer =
+        | System
+        | User of Username: string
+
+        override this.ToString() =
+            match this with
+            | System -> "DICE"
+            | User name -> name
+
+    type CommandData =
+        {
+            EntityId: Guid
+            Issuer: Issuer
+        }
+
+    type Command =
+        {
+            Data: CommandData
+            Body: obj
+        }
+
+module CommandBus =
+    open Commands
+    
+    let createCommandRouter handlers =
+        let event = Event<Command>()
+        handlers |> List.iter (fun h -> event.Publish |> Event.add h)
+
+        let agent = MailboxProcessor.Start(fun inbox -> 
+            let rec loop() = async {
+                let! cmd = inbox.Receive()
+                cmd |> event.Trigger
+                return! loop() }
+            loop())
+
+        { new IRouter<Command> with
+            member __.Send (cmd: Command) =
+                agent.Post cmd }
---
+++ VisualStudio/Simple.Web/TemplatePack/ProjectTemplates/Web/Simple.Web NEventStore/Domain Startup/EventBusStartup.fs
@@ -0,0 +1,47 @@
+﻿namespace $safeprojectname$.StartupTasks
+
+module EventBusStartup =
+    
+    open NEventStore
+    open NEventStore.Dispatcher
+    open NEventStore.Persistence.InMemoryPersistence
+    open NEventStore.Persistence.SqlPersistence.SqlDialects
+
+    [<RequireQualifiedAccess>]
+    module Serializer =
+        open Nessos.FsPickler
+        open NEventStore.Serialization
+
+        let createSerializer() =
+            let pickler = FsPickler()
+
+            { new ISerialize with
+                member __.Serialize<'T> (output, graph: 'T) =
+                    pickler.Serialize(output, graph) 
+                member __.Deserialize<'T> input =
+                    pickler.Deserialize<'T>(input) }
+
+    let createDispatcher handlers =
+        { new IDispatchCommits with
+            member __.Dispatch commit =
+                handlers |> List.iter (fun h -> h commit.StreamId commit.Events)
+            member __.Dispose() = () }
+
+    let createEventStore handlers =
+        let dispatcher = createDispatcher handlers
+
+        Wireup.Init()
+            .LogToConsoleWindow()
+            .UsingInMemoryPersistence()
+// -------------------------------------------------------------------------------------------------
+// To use SQL persistence, create a database and setup the "default" connection string in Web.Config
+// and uncomment the next three lines.
+//            .UsingSqlPersistence("default")
+//                .WithDialect(MsSqlDialect())
+//                .InitializeStorageEngine()
+// -------------------------------------------------------------------------------------------------
+            .UsingCustomSerialization(Serializer.createSerializer())
+                .Compress()
+            .UsingAsynchronousDispatchScheduler()
+                .DispatchTo(dispatcher)
+            .Build()
