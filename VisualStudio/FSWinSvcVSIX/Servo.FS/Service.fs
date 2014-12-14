6920b7d601f94b2b0c3f42ffe9b72571bcb6ca31
dc0d <kaveh.shahbazian@gmail.com>
2014-12-14 04:11:06 +0330

    F# Windows Service Project Template updated.

---
+++ VisualStudio/FSWinSvcVSIX/Servo.FS/Service.fs
@@ -0,0 +1,33 @@
+﻿module XService
+
+open NLog
+open Servo
+open System
+
+type XServiceService() =
+    static let ClassLogger = LogManager.GetCurrentClassLogger()
+
+    interface IService with
+        member this.OnContinue() = 
+            raise <| NotImplementedException()
+
+        member this.OnCustomCommand(command : int) = 
+            raise <| NotImplementedException()
+
+        member this.OnPause() = 
+            raise <| NotImplementedException()
+
+        member this.OnPowerEvent(powerStatus : System.ServiceProcess.PowerBroadcastStatus) = 
+            raise <| NotImplementedException()
+
+        member this.OnSessionChange(changeDescription : System.ServiceProcess.SessionChangeDescription) = 
+            raise <| NotImplementedException()
+
+        member this.OnShutdown() = 
+            raise <| NotImplementedException()
+
+        member this.OnStart(args : string[]) = 
+            ClassLogger.Info("Service Started - XService")
+
+        member this.OnStop() = 
+            ClassLogger.Info("Service Stopped - XService")
